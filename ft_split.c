c4bb14f2675749d4a459f1ff5ff828d83808553f
Samuel Guerra cardenas <sguerra-@c2r10s1.42madrid.com>
2021-10-28 15:32:39 +0200

    Finished project

---
+++ ft_split.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sguerra- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 18:02:13 by sguerra-          #+#    #+#             */
+/*   Updated: 2021/10/05 13:19:52 by sguerra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		t;
+	char	**tab;
+	int		split;
+
+	i = -1;
+	split = 0;
+	while (s && s[++i] != 0)
+		if ((s[i] != c && (i + 1 == (int)ft_strlen(s) || s[i + 1] == c)))
+			split++;
+	tab = ft_calloc(split + 1, sizeof(char *));
+	if (!tab || !s)
+		return (NULL);
+	t = split;
+	split = i - 1;
+	while (i > 0)
+	{
+		if (s[i - 1] != c && s[i] == c)
+			split = i - 1;
+		if ((s[i - 1] == c && s[i] != c) || (i == 1 && s[--i] != c))
+			tab[--t] = ft_substr(s, i, split - i + 1);
+		i--;
+	}
+	return (tab);
+}
